344439c2b6f3180be3c3b287a86e33575b1e0e77
laylathias <112867789+laylathias@users.noreply.github.com>
2022-09-12 17:02:57 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    printf("Test");
+    /*
+    int=integer
+    character
+    x bisa diubah fariabel lain */
+    int x = 18;
+    float t = 168.5;
+    char b = 'L';
+    string ub = "xxl";
+    cout << "Umur saya: " << x;
+    cout << endl;
+    cout << "Tinggi saya: " << t;
+    cout << "\nUkuran baju: " << b;
+    cout << "\nUkuran baju: " << ub;
+    return 0;
+}
