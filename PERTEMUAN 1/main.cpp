0b000d8f8dc85883d33585161df4d6778909673b
laylathias <112867789+laylathias@users.noreply.github.com>
2022-09-12 17:02:36 +0700

    Add files via upload

---
+++ PERTEMUAN 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAMA: LAYLA THIAS RAHMAWATI" << endl;
+    cout << "NIM: A11.2022.14132" << endl;
+    cout << "ALAMAT: Ds. Gambuhan, Kec. Pulosari, Kab. Pemalang, Jawa Tengah" << endl;
+    cout << "ASAL SEKOLAH: SMA NEGERI 1 MOGA" << endl;
+    return 0;
+}
